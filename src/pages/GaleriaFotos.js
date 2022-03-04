a37ef3861c7beacf4eb7bbf51ae917fba922e031
ADMIN <ADMIN@MacBook-Air.home>
2022-03-04 08:34:35 +0100

    04032022

--- src/pages/GaleriaFotos.js
+++ src/pages/GaleriaFotos.js
@@ -2,15 +2,64 @@
 // 2.En clicar un foto, mostrar-la en pantalla completa (avançat)
 // 3.En clicar la foto ampliada, mostrar la graella un altre cop
 
-
-import { useState } from "react";
 import "./GaleriaFotos.css";
-
+import { useState } from "react";
 
 export default function GaleriaFotos() {
+  const [indexFotoActual, setIndexFotoActual] = useState(0);
+
+  const visualiza = (foto) => setIndexFotoActual(foto);
+
+  if (indexFotoActual) {
     return (
-        <div className="Graella">
-            <img src="../../public/img/1.jpg" alt="Cerdenya" /> 
-        </div>
+      <img
+        id="ampliada"
+        src={process.env.PUBLIC_URL + "/img/" + indexFotoActual + ".jpg"}
+        onClick={() => visualiza(0)}
+        alt="Imagen Actual"
+      ></img>
     );
+  }
+  return (
+    <>
+      <body class="Galeria">
+        <div class="general">
+          <header>
+            <h1>Práctica Galeria de fotografias</h1>
+          </header>
+          <main class="detalle">
+            <img
+              onClick={() => visualiza(1)}
+              src={process.env.PUBLIC_URL + "/img/1.jpg"}
+              alt="Imagen 1"
+            ></img>
+            <img
+              onClick={() => visualiza(2)}
+              src={process.env.PUBLIC_URL + "/img/2.jpg"}
+              alt="Imagen 2"
+            ></img>
+            <img
+              onClick={() => visualiza(3)}
+              src={process.env.PUBLIC_URL + "/img/3.jpg"}
+              alt="Imagen 3"
+            ></img>
+            <img
+              onClick={() => visualiza(4)}
+              src={process.env.PUBLIC_URL + "/img/4.jpg"}
+              alt="Imagen 4"
+            ></img>
+            <img
+              onClick={() => visualiza(5)}
+              src={process.env.PUBLIC_URL + "/img/5.jpg"}
+              alt="Imagen 5"
+            ></img>
+          </main>
+        </div>
+      </body>
+
+      <footer>
+        <p>Final</p>
+      </footer>
+    </>
+  );
 }
