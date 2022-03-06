2f8aeaee837d8132e188da01680a1a93d0d5c014
jordi <jpratsv@gmail.com>
2022-03-06 22:53:36 +0100

    05/03/2022 22:53

---
+++ src/pages/Menu.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./Garaje.css";
+import Garaje from "./Garaje";
+
+export default function Menu() {
+  const [buttonText, setButtonText] = useState("Laborables");
+  const [buttonColor, setButtonColor] = useState("blue");
+
+  const changeText = () => {
+    buttonText === "Laborables"
+      ? setButtonText("Festivos")
+      : setButtonText("Laborables");
+
+    buttonColor === "blue" ? setButtonColor("red") : setButtonColor("blue");
+  };
+
+  let cars =
+    buttonText === "Laborables"
+      ? ["Toyota", "Citroen", "Seat", "Renault"]
+      : ["Ferrary", "Tesla", "Mercedes"];
+
+  let coches = cars;
+  let tipo = buttonText;
+  
+
+  return (
+    <div className="Menu">
+      <h1> ¿Que coche me toca hoy? </h1>
+
+      <button onClick={changeText} className={buttonColor}>
+        {buttonText}
+      </button>
+
+      <Garaje coches={coches} tipos={tipo} />
+    </div>
+  );
+}
--- src/pages/llista.js
+++ src/pages/llista.js
@@ -2,11 +2,16 @@ import GaleriaFotos from './GaleriaFotos';
 import Acordeon from './Acordeon';
 import Comptador from './Comptador';
 import ToggleButton from './ToggleButton';
+import DisplayCalculadora from './DisplayCalculadora'
+import Menu from './Menu.js'
 
 const llista = [
 {path: 'galeriaFotos', titol:'Galeria de Fotos', element: <GaleriaFotos/>},
 {path: 'acordeon', titol:'Acordeón', element: <Acordeon/>},
 {path: 'comptador', titol:'Contador', element: <Comptador/>},
 {path: 'toggle-button', titol:'Toogle button', element: <ToggleButton/>},
+{path: 'calculadora', titol:'Displaya Calculadora', element: <DisplayCalculadora/>},
+{path: 'menu', titol:'Garaje', element: <Menu/>},
+
 ];
 export default llista;
