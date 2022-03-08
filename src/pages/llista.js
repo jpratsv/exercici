d3870e89325518347306f45aca1a609bd6939eb7
ADMIN <ADMIN@MacBook-Air.local>
2022-03-08 19:32:15 +0100

    08/03/2022 19:32

--- src/pages/llista.js
+++ src/pages/llista.js
@@ -1,21 +1,30 @@
-import GaleriaFotos from './GaleriaFotos';
-import Acordeon from './Acordeon';
-import Comptador from './Comptador';
-import ToggleButton from './ToggleButton';
-import DisplayCalculadora from './DisplayCalculadora'
-import Menu from './Menu'
-import EntraArray from './EntraArray'
-import AfegirElement from './AfegirElement'
+import GaleriaFotos from "./GaleriaFotos";
+import Acordeon from "./Acordeon";
+import Comptador from "./Comptador";
+import ToggleButton from "./ToggleButton";
+import DisplayCalculadora from "./DisplayCalculadora";
+import Menu from "./Menu";
+import EntraArray from "./EntraArray";
+import AfegirElement from "./AfegirElement";
+import EjercicioTres from "./EjercicioTres";
 
 const llista = [
-{path: 'galeriaFotos', titol:'Galeria de Fotos', element: <GaleriaFotos/>},
-{path: 'acordeon', titol:'Acordeón', element: <Acordeon/>},
-{path: 'comptador', titol:'Contador', element: <Comptador/>},
-{path: 'toggle-button', titol:'Toogle button', element: <ToggleButton/>},
-{path: 'calculadora', titol:'Displaya Calculadora', element: <DisplayCalculadora/>},
-{path: 'menu', titol:'Garaje', element: <Menu/>},
-{path: 'entraArray', titol:'Exercici-1 ', element: <EntraArray/>},
-{path: 'AfegirElement', titol:'Exercici-2 ', element: <AfegirElement/>},
-
+  {
+    path: "galeriaFotos",
+    titol: "Galeria de Fotos",
+    element: <GaleriaFotos />,
+  },
+  { path: "acordeon", titol: "Acordeón", element: <Acordeon /> },
+  { path: "comptador", titol: "Contador", element: <Comptador /> },
+  { path: "toggle-button", titol: "Toogle button", element: <ToggleButton /> },
+  {
+    path: "calculadora",
+    titol: "Displaya Calculadora",
+    element: <DisplayCalculadora />,
+  },
+  { path: "menu", titol: "Garaje", element: <Menu /> },
+  { path: "entraArray", titol: "Exercici-1 ", element: <EntraArray /> },
+  { path: "AfegirElement", titol: "Exercici-2 ", element: <AfegirElement /> },
+  { path: "EjercicioTres", titol: "Exercici-3 ", element: <EjercicioTres /> },
 ];
 export default llista;
